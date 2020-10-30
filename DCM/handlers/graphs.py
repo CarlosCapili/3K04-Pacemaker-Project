a5bdeca938d1229faa760189d86b676f29efb170
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2020-10-30 01:24:39 -0400

    graphs

--- DCM/handlers/graphs.py
+++ DCM/handlers/graphs.py
@@ -13,28 +13,32 @@ class GraphsHandler(QThread):
         self.atri_data_sense = np.random.normal(size=100)
         self.vent_data_sense = np.random.normal(size=100)
 
-        self.atri_pace_plot = atri_plot.plot()
-        self.atri_sense_plot = atri_plot.plot()
+        self.atri_pace_plot = atri_plot.plot(pen=(252, 93, 93))
+        self.atri_sense_plot = atri_plot.plot(pen=(0, 229, 255))
 
-        self.vent_pace_plot = vent_plot.plot()
-        self.vent_sense_plot = vent_plot.plot()
+        self.vent_pace_plot = vent_plot.plot(pen=(252, 93, 93))
+        self.vent_sense_plot = vent_plot.plot(pen=(0, 229, 255))
 
     def pace_plot(self):
-        print("pace plot")
-        self.atri_pace_plot.setData(self.atri_data_pace, pen=(252, 93, 93))
-        self.vent_pace_plot.setData(self.vent_data_pace, pen=(252, 93, 93))
+        self.atri_pace_plot.setData(self.atri_data_pace)
+        self.vent_pace_plot.setData(self.vent_data_pace)
 
     def sense_plot(self):
-        print("sense plot")
-        self.atri_sense_plot.setData(self.vent_data_sense, pen=(0, 229, 255))
-        self.vent_sense_plot.setData(self.atri_data_sense, pen=(0, 229, 255))
+        self.atri_sense_plot.setData(self.vent_data_sense)
+        self.vent_sense_plot.setData(self.atri_data_sense)
 
-    def pace_clear(self):
-        print("pace clear")
-        self.atri_pace_plot.clear()
-        self.vent_pace_plot.clear()
+    def pace_show(self):
+        self.atri_sense_plot.show()
+        self.vent_sense_plot.show()
 
-    def sense_clear(self):
-        print("sense clear")
-        self.atri_sense_plot.clear()
-        self.vent_sense_plot.clear()
+    def sense_show(self):
+        self.atri_sense_plot.show()
+        self.vent_sense_plot.show()
+
+    def pace_hide(self):
+        self.atri_pace_plot.hide()
+        self.vent_pace_plot.hide()
+
+    def sense_hide(self):
+        self.atri_sense_plot.hide()
+        self.vent_sense_plot.hide()
