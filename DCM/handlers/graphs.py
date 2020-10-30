b5f381e59843346a20510458dcbd28f524ad4a9e
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2020-10-29 20:45:29 -0400

    leads, graphs

--- DCM/handlers/graphs.py
+++ DCM/handlers/graphs.py
@@ -7,12 +7,16 @@ class GraphsHandler(QThread):
         super().__init__()
         print("Graphs handler init")
 
-        self.atri_data = np.random.normal(size=300)
-        self.vent_data = np.random.normal(size=300)
+        self.atri_data_pace = np.random.normal(size=100)
+        self.vent_data_pace = np.random.normal(size=100)
+        self.atri_data_sense = np.random.normal(size=100)
+        self.vent_data_sense = np.random.normal(size=100)
 
         self.atri_plot = atri_plot
         self.vent_plot = vent_plot
 
     def plot(self):
-        self.atri_plot.plot(self.atri_data)
-        self.vent_plot.plot(self.vent_data)
+        self.atri_plot.plot(self.atri_data_pace, pen=(252, 93, 93))
+        self.atri_plot.plot(self.atri_data_sense, pen=(0, 229, 255))
+        self.vent_plot.plot(self.vent_data_pace, pen=(252, 93, 93))
+        self.vent_plot.plot(self.vent_data_sense, pen=(0, 229, 255))
